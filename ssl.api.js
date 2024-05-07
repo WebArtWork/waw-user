eeef90366149468ea65574ab5674e0cae4ed25a6
Narolskyi Denis <asotizianov@gmail.com>
2024-05-07 17:31:20 +0300

    fixed issues with product url

--- ssl.api.js
+++ ssl.api.js
@@ -1,20 +1,11 @@
+
 module.exports = function (waw) {
-	if (!waw.config.ssls) {
-		return;
-	}
-	const serve = (page, config) => {
-		return (req, res) => {
-			res.send(config.code);
-		};
-	};
-	for (const ssl of waw.config.ssls) {
-		const page = {};
-		for (const config of ssl.configs) {
-			page[config.url] = serve(page, config);
-		}
-		waw.api({
-			domain: ssl.domain,
-			page,
-		});
-	}
-};
+  waw.api({
+    domain: 'waiter.cloud',
+    get: {
+      '/.well-known/acme-challenge/Me92yJzRwICOt_saIfdMcWUThHwBPSxAJ9KuZTPyAZc': (req, res)=>{
+        res.send('Me92yJzRwICOt_saIfdMcWUThHwBPSxAJ9KuZTPyAZc.Ckbi6rboZJC4m3tVJdZ7BpqcZ9DzMQefzvDTgFU3e1o');
+      }
+    }
+  })
+}
